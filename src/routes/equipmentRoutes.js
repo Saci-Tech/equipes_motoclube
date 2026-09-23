3a7c14124ce8d403d6830937ce3cd16dd0c9b76f
Eduardo Barbosa <e.barbosa.83@outlook.com>
2026-09-23 14:20:18 -0300

    Added some controllers and their tests

---
+++ src/routes/equipmentRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const equipmentController = require('../controllers/EquipmentController');
+
+router.get('/', equipmentController.getAll);
+router.get('/:id', equipmentController.getById);
+router.get('/serial/:serialNumber', equipmentController.getBySerialNumber);
+router.get('/category/:category', equipmentController.getByCategory);
+router.get('/status/:status', equipmentController.getByStatus);
+router.post('/', equipmentController.create);
+router.put('/:id', equipmentController.update);
+router.delete('/:id', equipmentController.delete);
+
+module.exports = router;
---
+++ src/routes/presenceRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const presenceController = require('../controllers/PresenceController');
+
+router.get('/', presenceController.getAll);
+router.get('/:id', presenceController.getById);
+router.get('/event/:eventId', presenceController.getByEvent);
+router.get('/member/:memberId', presenceController.getByMember);
+router.post('/', presenceController.create);
+router.put('/:id', presenceController.update);
+router.delete('/:id', presenceController.delete);
+
+module.exports = router;
